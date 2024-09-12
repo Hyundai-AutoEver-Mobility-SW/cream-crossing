c1749c5d33111301aad749d210ea33462f669226
vinivin153 <vinivin153@naver.com>
2024-09-12 16:08:14 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <h1>그남자그여자3</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1300px",
+      },
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+};
