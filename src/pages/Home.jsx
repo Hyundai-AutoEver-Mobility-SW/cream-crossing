e374bb4fcc5ae27a982abf255512e1007b28ba70
hyejeong <67236054+softourr@users.noreply.github.com>
2024-09-14 00:17:08 +0900

    [feat] home product layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import Hero from '../components/home/Hero';
 import TopAnimal from '../components/home/TopAnimal';
 import Banner from '../components/home/Banner';
 import Banner2 from '../components/home/Banner2';
 import Categories from '../components/home/Categories';
+import styled from 'styled-components';
+import Products from '../\bcomponents/home/Products';
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1280px;
+  background-color: #fff;
+`;
 
 const Home = () => {
   return (
@@ -13,6 +20,10 @@ const Home = () => {
       <Banner />
       <TopAnimal />
       <Banner2 />
+      {/*  */}
+      <Container>
+        <Products />
+      </Container>
     </div>
   );
 };
