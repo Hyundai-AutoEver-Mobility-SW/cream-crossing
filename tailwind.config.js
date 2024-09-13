a6c29768c3ffc3c5cbba17944fabd9ab23c46c79
hyejeong <67236054+softourr@users.noreply.github.com>
2024-09-13 21:04:13 +0900

    [build] tailwind error fix, favicon
    - uninstall tailwind > install tainwindcss
    - add postcss, autuprefixer
    - set favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    container: {
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1300px",
-      },
-    },
-    extend: {
-      colors: {},
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
