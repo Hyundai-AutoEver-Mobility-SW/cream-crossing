92ac32f7b20e12131c68cc36370c43c37f42b7e4
zlnongi <yeossi8@naver.com>
2024-09-14 16:03:01 +0900

    [feat] Cart-tabs UI

--- src/components/basekt/Basket.jsx
+++ src/components/basekt/Basket.jsx
@@ -6,10 +6,15 @@ function Basket() {
       <div className="container mx-auto cart-top">
         <h1 className="font-bold text-[1.5em] p-4 text-center w-full bg-red-200">장바구니</h1>
 
-        <div className="content-area h-[60px]">
-          <div className="cart-tabs">
-            <div className="cart-tab w-[700px]">
-              <div>2</div>
+        <div className="content-area h-[60px] flex justify-center items-center">
+          <div className="cart-tabs  w-[700px] text-center flex relative bg-indigo-100">
+            <div className="cart-tab active relative h-[60px] w-1/2 cursor-pointer p-2 border-b border-gray-300">
+              <div className="count">2</div>
+              <div className="title font-semibold text-[13px]">Kream 배송</div>
+            </div>
+            <div className="cart-tab2 relative h-[60px] w-1/2 cursor-pointer p-2 border-b border-gray-300">
+              <div className="count">0</div>
+              <div className="title font-semibold text-[13px]">브랜드 배송</div>
             </div>
           </div>
         </div>
